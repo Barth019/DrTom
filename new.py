1bd0ea609bff68101a2c8f98554a545a061871b4
Ebrahimkhaleel <ibrahimkhaleel0123@gmail.com>
2024-01-29 11:35:25 +0100

    new

--- new.py
+++ new.py
@@ -1,3 +1,25 @@
-from keras.models import Sequential
-from keras.layers import Dense
-import tensorFlow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+train_datagen = ImageDataGenerator(rescale=1./255)
+
+train_generator = train_datagen.flow_from_directory(
+    r'C:\Users\KHERLEELU\Desktop\projects\python\potato',
+    class_mode='binary'
+)
+
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow.keras import models,layers
+
+IMAGE_SIZE=256
+BATCH_SIZE=32
+
+image_dataset=tf.keras.preprocessing.image_dataset_from_directory(
+   'potato',
+   shuffle=True,
+   image_size=(IMAGE_SIZE,IMAGE_SIZE),
+   batch_size=(BATCH_SIZE)
+)
+class_names=image_dataset.class_names
+print(class_names)
